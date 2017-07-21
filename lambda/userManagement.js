0d1733a89838e49d0c12367e0b92e222ca69c3ac
root <horvath.peter.hp@gmail.com>
2017-07-21 14:56:23 +0000

    Added all methods to backend

--- lambda/userManagement.js
+++ lambda/userManagement.js
@@ -2,7 +2,10 @@ var doc = require('dynamodb-doc');
 var crypto = require('crypto');
 
 var dynamodb = new doc.DynamoDB();
-var tableName = 'holiday-users'
+var tableName = 'holiday-users';
+
+//defines the fields that can be set and updated for a user
+var openFields = ['email', 'forename', 'surname'];
 
 var commonDebug = (event) => {
     var resultString = 'Method was: ' + event.httpMethod;
@@ -11,7 +14,7 @@ var commonDebug = (event) => {
         resultString += ' userid: ' + event.pathParameters.userid;
     }
     return resultString;
-}
+};
 
 var makeDummyObject = (id) => {
     return {
@@ -21,7 +24,7 @@ var makeDummyObject = (id) => {
         surname: 'Surname',
         created: 'today'
     };
-}
+};
 
 /**
  * Tests string to only contain letters, ' and - and spaces, and should start
@@ -30,39 +33,62 @@ var makeDummyObject = (id) => {
  */
 var nameChecker = (str) => {
     return /^[a-zA-Z]+[a-zA-Z \-']+$/.test(str);
-}
+};
 
 /**
  * Tests string to only contain hexadecimal characters
  */
 var hashChecker = (str) => {
     return /^[abcdef0-9]+$/.test(str);
-}
+};
 
 /**
- * Validates the inputs
+ * Checks for valid id. Returns true for valid, false for invalid.
  */
-var validateNewUser = (data) => {
+var validateHash = (event) => {
+    //check for valid id
+    return typeof event.pathParameters === 'object' && 
+        typeof event.pathParameters.userid === 'string' &&
+        hashChecker(event.pathParameters.userid);
+};
+
+/**
+ * Validates the input for a user
+ * if needAll is true, presence will be enforced
+ */
+var validateUserData = (data, needAll) => {
     var result = '';
     
     if (typeof data !== 'object'){
         return 'No data or wrong format';
     }
-    if (typeof data.email !== 'string' || !data.email.length || data.email.indexOf('@') === -1){
-        result = 'Missing email or wrong format';
+    if (data.email || needAll){
+        if (typeof data.email !== 'string' ||
+            !data.email.length || data.email.indexOf('@') === -1){
+
+            result = 'Bad email supplied ';
+        }
     }
-    if (typeof data.forename !== 'string' || !data.forename.length || !nameChecker(data.forename)){
-        result += 'Missing forename or wrong format';
+    if (data.forename || needAll){
+        if (typeof data.forename !== 'string' ||
+            !data.forename.length || !nameChecker(data.forename)){
+    
+            result += 'Bad forename supplied ';
+        }
     }
-    if (typeof data.surname !== 'string' || !data.surname.length || !nameChecker(data.forename)){
-        result += 'Missing surname or wrong format';
+    if (data.surname || needAll){
+        if (typeof data.surname !== 'string' ||
+            !data.surname.length || !nameChecker(data.forename)){
+    
+            result += 'Bad surname supplied';
+        }
     }
     if (result.length){
         return result;
     } else {
         return null;
     }
-}
+};
 
 /**
  * Creates a new user object from the submitted data.
@@ -80,15 +106,23 @@ var userFromData = (data) => {
         surname: data.surname,
         created: date
     };
-}
+};
 
 /**
  * Creates a new user from the input parameters
+ * POST /users
+ * body {
+ *   "email": "email@random.com",
+ *   "forename": "Forename",
+ *   "surname": "Surname"
+ * }
  */
 var handlePOST = (event, result, context, callback) => {
     var submittedData = JSON.parse(event.body);
     //validate input data and send back error if malformed
-    var validationResult = validateNewUser(submittedData);
+    var validationResult = validateUserData(submittedData, true);
+    var newUser;
+    
     if(validationResult){
         result.body = validationResult;
         result.statusCode = 400;
@@ -96,7 +130,7 @@ var handlePOST = (event, result, context, callback) => {
         return;
     }
     
-    var newUser = userFromData(submittedData);
+    newUser = userFromData(submittedData);
 
     dynamodb.putItem({
         "TableName": tableName,
@@ -114,22 +148,221 @@ var handlePOST = (event, result, context, callback) => {
             result.statusCode = 201;
             result.headers = {
                 Location: event.path + '/' + newUser.userid
-            }
+            };
             callback(null, result);
         }
     });
             
-}
+};
+
+/**
+ * Used to handle email query
+ */
+var handleNoId = (event, result, context, callback) => {
+    var email = event.queryStringParameters.email;
+    var params;
+    if(email){
+        
+        //result.body = event.queryStringParameters.email;
+        //callback(null, result);
+        //return;
+        
+        params = {
+            TableName : tabelName,
+            KeyConditionExpression: "#em = :email",
+            ExpressionAttributeNames:{
+                "#em": "email"
+            },
+            ExpressionAttributeValues: {
+                ":email": event.queryStringParameters.email
+            }
+        };
+
+        dynamodb.query(params, (err, data) => {
+            if (err) {
+                console.error('Error getting item from dynamodb: ' + err);
+                result.body = 'Error getting item from dynamodb: ' + err;
+                result.statusCode = 500;
+                callback(null, result);
+            }
+            else {
+                console.log('success: ' + JSON.stringify(data, null, '  '));
+                //handle not found
+                if (!data.Items.length){
+                    result.statusCode = 404;
+                    callback(null, result);
+                    return;
+                }
+                result.body = data.Items.length + '';
+                //result.body = JSON.stringify(data.Items[0]);
+                result.statusCode = 200;
+                callback(null, result);
+            }
+        });
+    } else {
+        result.statusCode = 400;
+        callback(null, result);
+        return;
+    }
+};
 
 /**
  * Retrieves the user for the given id in json
+ * GET /users/<userid>
  */ 
 var handleGET = (event, result, context, callback) => {
+    //handle no id - list all
+    if (event.path === '/users'){
+        handleNoId(event, result, context, callback);
+        return;
+    }
+
+    //check for valid id
+    if (!validateHash(event)){
+        result.statusCode = 400;
+        callback(null, result);
+    }
+    
+    dynamodb.getItem({
+        "TableName": tableName,
+        "Key" : {
+            userid: event.pathParameters.userid
+        }
+    }, (err, data) => {
+        if (err) {
+            console.error('Error getting item from dynamodb: ' + err);
+            result.body = 'Error getting item from dynamodb: ' + err;
+            result.statusCode = 500;
+            callback(null, result);
+        }
+        else {
+            console.log('success: ' + JSON.stringify(data, null, '  '));
+            //handle not found
+            if (!Object.keys(data).length){
+                result.statusCode = 404;
+                callback(null, result);
+                return;
+            }
+            result.body = JSON.stringify(data.Item);
+            result.statusCode = 200;
+            callback(null, result);
+        }
+    });
+};
+
+/**
+ * Updates the user's attributes
+ * PUT /users/<userid>
+ * 
+ * all fields are optional in body:
+ * body {
+ *   "email": "email@random.com",
+ *   "forename": "Forename",
+ *   "surname": "Surname"
+ * }
+ */ 
+var handlePUT = (event, result, context, callback) => {
+    var submittedData;
+    var validationResult;
+    var updates;
+    var i;
+
+    //check for valid id
+    if (!validateHash(event)){
+        result.statusCode = 400;
+        callback(null, result);
+    }
+    
+    submittedData = JSON.parse(event.body);
+    //validate input data and send back error if malformed
+    validationResult = validateUserData(submittedData, false);
+
+    if(validationResult){
+        result.body = validationResult;
+        result.statusCode = 400;
+        callback(null, result);
+        return;
+    }
+    
+    //create updates object for db update
+    updates = {};
+    for (i = 0; i < openFields.length; ++i){
+        if (submittedData[openFields[i]]){
+            updates[openFields[i]] = {
+                Action: 'PUT',
+                Value: submittedData[openFields[i]]
+            }
+        }
+    }
+    
+    //if no updates, we are finished
+    if (!Object.keys(updates).length){
+        result.statusCode = 200;
+        callback(null, result);
+        return;
+    }
+    
+    //update dynamo
+    dynamodb.updateItem({
+        "TableName": tableName,
+        "Key": {
+            userid: event.pathParameters.userid
+        },
+        AttributeUpdates: updates
+    }, (err, data) => {
+        if (err) {
+            console.error('Error updating item in dynamodb: ' + err);
+            result.body = 'Error updating item in dynamodb: ' + err;
+            result.statusCode = 500;
+            callback(null, result);
+        }
+        else {
+            console.log('success: ' + JSON.stringify(data, null, '  '));
+            result.body = JSON.stringify(data.Item);
+            result.statusCode = 200;
+            callback(null, result);
+        }
+    });
+    
+};
+/**
+ * Handles deleting users for a given id
+ * DELETE /users/<userid>
+ * 
+ * Will return 200 on deleting non-existing records
+ */ 
+var handleDELETE = (event, result, context, callback) => {
+    //check for valid id
+    if (!validateHash(event)){
+        result.statusCode = 400;
+        callback(null, result);
+    }
+    
+    //delete from dynamo
+    dynamodb.deleteItem({
+        "TableName": tableName,
+        "Key": {
+            userid: event.pathParameters.userid
+        }
+    }, (err, data) => {
+        if (err) {
+            console.error('Error deleting item from dynamodb: ' + err);
+            result.body = 'Error deleting item from dynamodb: ' + err;
+            result.statusCode = 500;
+            callback(null, result);
+        }
+        else {
+            console.log('success: ' + JSON.stringify(data, null, '  '));
+            result.body = JSON.stringify(data.Item);
+            result.statusCode = 200;
+            callback(null, result);
+        }
+    });
     
 }
 
 exports.handler = (event, context, callback) => {
-    console.log(commonDebug(event));
+    //console.log(commonDebug(event));
     var result = {
         'isBase64Encoded': false,
         'statusCode': 200,
@@ -139,19 +372,16 @@ exports.handler = (event, context, callback) => {
     
     switch(event.httpMethod){
         case "GET":
-            result.body = JSON.stringify(makeDummyObject(event.pathParameters.userid));
-            callback(null, result);
+            handleGET(event, result, context, callback);
             break;
         case "PUT":
-            result.body = "PUT unimplemented";
-            callback(null, result);
+            handlePUT(event, result, context, callback);
             break;
         case "POST":
             handlePOST(event, result, context, callback);
             break;
         case "DELETE":
-            result.body = "DELETE unimplemented";
-            callback(null, result);
+            handleDELETE(event, result, context, callback);
             break;
         default:
             result.statusCode = 504;
